0a690e46c60d076afb92c3e837f6caa9d06caf9b
Kyle Mututo <135003824+Se7enseads@users.noreply.github.com>
2023-09-05 17:08:38 +0300

    Completed DSA assignment.

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-def remove_duplicates(sequence):
-    seen = []
-    result = []
-
-    for item in sequence:
-        if item not in seen:
-            seen.append(item)
-            result.append(item)
-
-    return result
-
-
-input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
-result = remove_duplicates(input_sequence)
-# print(result)
+import re
 
 
 def is_balanced(expression):
@@ -28,3 +14,31 @@ def is_balanced(expression):
             stack.append(char)
 
     return not stack
+
+
+def remove_duplicates(sequence):
+    seen = []
+    result = []
+
+    for item in sequence:
+        if item not in seen:
+            seen.append(item)
+            result.append(item)
+
+    return result
+
+
+def word_frequency(sentence):
+    word_pattern = r'\b\w+\b'
+
+    words = re.findall(word_pattern, sentence.lower())
+
+    word_count = {}
+
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+
+    return word_count
--- main_test.py
+++ main_test.py
@@ -1,19 +1,19 @@
 import unittest
 
-from main import remove_duplicates, is_balanced
+from main import remove_duplicates, is_balanced, word_frequency
 
 
 class TestFunctions(unittest.TestCase):
 
     def test_remove_duplicates(self):
         input_sequence1 = [2, 3, 2, 4, 5, 3, 6, 7, 5]
-        result1 = remove_duplicates(input_sequence1)
+        result1 = [2, 3, 4, 5, 6, 7]
 
         input_sequence2 = [3, 3, 5, 6, 7, 2, 3, 5, 6, 6, 7, 7]
-        result2 = remove_duplicates(input_sequence2)
+        result2 = [3, 5, 6, 7, 2]
 
-        self.assertEqual(result1, [2, 3, 4, 5, 6, 7])
-        self.assertEqual(result2, [3, 5, 6, 7, 2])
+        self.assertEqual(remove_duplicates(input_sequence1), result1)
+        self.assertEqual(remove_duplicates(input_sequence2), result2)
 
     def test_is_balanced(self):
         expression1 = "([]{})"
@@ -25,6 +25,18 @@ class TestFunctions(unittest.TestCase):
         self.assertFalse(is_balanced(expression2))
         self.assertTrue(is_balanced(expression3))
         self.assertTrue(is_balanced(expression4))
+
+    def test_word_frequency(self):
+        sentence1 = "This is a test sentence. This sentence is a test."
+        result1 = {'this': 2, 'is': 2,
+                   'a': 2, 'test': 2, 'sentence': 2}
+
+        sentence2 = "The quick brown fox jumps over the lazy dog."
+        result2 = {'the': 2, 'quick': 1, 'brown': 1, 'fox': 1,
+                   'jumps': 1, 'over': 1,   'lazy': 1, 'dog': 1}
+
+        self.assertEqual(word_frequency(sentence1), result1)
+        self.assertEqual(word_frequency(sentence2), result2)
 
 
 if __name__ == '__main__':
